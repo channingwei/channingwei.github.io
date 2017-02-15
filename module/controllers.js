22eed5213f3d4faa4b7aef046aa08b1917a40ef0
Channing Kuo <492689363@qq.com>
2017-02-15 21:44:54 +0800

    homepage redesign

--- module/controllers.js
+++ module/controllers.js
@@ -7,11 +7,19 @@
 (function () {
    'use strict';
 
-   ckApp.controller("HomeController", ['$scope', '$filter', '$state', '$compile', 'rt', '$ionicScrollDelegate', HomeController]);
-   function HomeController($scope, $filter, $state, $compile, rt, $ionicScrollDelegate) {
+   ckApp.controller("HomeController", ['$scope', '$filter', '$state', '$compile', 'rt', '$ionicScrollDelegate', 'HomeService', HomeController]);
+   function HomeController($scope, $filter, $state, $compile, rt, $ionicScrollDelegate, HomeService) {
 
       function init() {
          $scope.screenHeight = rt.getScreenHeight();
+
+         // document.getElementById('close').onmousedown = function(e) {
+         //    e.preventDefault();
+         //    document.getElementById('info').style.display = 'none';
+         //    return false;
+         // };
+
+         // HomeService.init('c', 800, 800);
       }
 
       init();
