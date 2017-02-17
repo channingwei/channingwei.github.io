c73a2ea22ff65eb4b54bfb7660e1baa0db3bf826
ChanningKuo <492689363@qq.com>
2017-02-17 13:37:10 +0800

    add star watch fork iframe and add position animation

--- assets/js/share.js
+++ assets/js/share.js
@@ -8,54 +8,62 @@ $(document).ready(function(){
 		,spacing=75
 	;
 
-	function openShareMenu(){
-		TweenMax.to($toggleButton,0.1,{
-			scaleX:1.2,
-			scaleY:0.6,
-			ease:Quad.easeOut,
-			onComplete:function(){
-				TweenMax.to($toggleButton,.8,{
+	function openShareMenu(firstFire){
+		if(!firstFire){
+			// 变更图标的位置————先移动位置再打开按钮
+			$(".content").removeClass("content-position");
+			$(".content").css("transition","1s");
+		}
+		setTimeout(function(){
+			TweenMax.to($toggleButton,0.1,{
+				scaleX:1.2,
+				scaleY:0.6,
+				ease:Quad.easeOut,
+				onComplete:function(){
+					TweenMax.to($toggleButton,.8,{
+						scale:0.6,
+						ease:Elastic.easeOut,
+						easeParams:[1.1,0.6]
+					})
+					TweenMax.to($toggleButton.children(".share-icon"),.8,{
+						scale:1.4,
+						ease:Elastic.easeOut,
+						easeParams:[1.1,0.6]
+					})
+				}
+			})
+			$shareButtons.each(function(i){
+				var $cur=$(this);
+				var pos=i-buttonsMid;
+				if(pos>=0) pos+=1;
+				var dist=Math.abs(pos);
+				$cur.css({
+					zIndex:buttonsMid-dist
+				});
+				TweenMax.to($cur,1.1*(dist),{
+					x:pos*spacing,
+					scaleY:0.6,
+					scaleX:1.1,
+					ease:Elastic.easeOut,
+					easeParams:[1.01,0.5]
+				});
+				TweenMax.to($cur,.8,{
+					delay:(0.2*(dist))-0.1,
 					scale:0.6,
 					ease:Elastic.easeOut,
 					easeParams:[1.1,0.6]
 				})
-				TweenMax.to($toggleButton.children(".share-icon"),.8,{
-					scale:1.4,
-					ease:Elastic.easeOut,
-					easeParams:[1.1,0.6]
-				})
-			}
-		})
-		$shareButtons.each(function(i){
-			var $cur=$(this);
-			var pos=i-buttonsMid;
-			if(pos>=0) pos+=1;
-			var dist=Math.abs(pos);
-			$cur.css({
-				zIndex:buttonsMid-dist
-			});
-			TweenMax.to($cur,1.1*(dist),{
-				x:pos*spacing,
-				scaleY:0.6,
-				scaleX:1.1,
-				ease:Elastic.easeOut,
-				easeParams:[1.01,0.5]
-			});
-			TweenMax.to($cur,.8,{
-				delay:(0.2*(dist))-0.1,
-				scale:0.6,
-				ease:Elastic.easeOut,
-				easeParams:[1.1,0.6]
-			})
 
-			TweenMax.fromTo($cur.children(".share-icon"),0.2,{
-				scale:0
-			},{
-				delay:(0.2*dist)-0.1,
-				scale:1,
-				ease:Quad.easeInOut
+				TweenMax.fromTo($cur.children(".share-icon"),0.2,{
+					scale:0
+				},{
+					delay:(0.2*dist)-0.1,
+					scale:1,
+					ease:Quad.easeInOut
+				})
 			})
-		})
+		}, 400);
+
 	}
 	function closeShareMenu(){
 		TweenMax.to([$toggleButton,$toggleButton.children(".share-icon")],1.4,{
@@ -84,14 +92,16 @@ $(document).ready(function(){
 				ease:Quad.easeIn
 			});
 		})
+
+		// 变更图标的位置————先合并在移动到右下角
+		$(".content").addClass("content-position");
 	}
 
 	function toggleShareMenu(){
 		menuOpen=!menuOpen
-		menuOpen?openShareMenu():closeShareMenu();
-		// TODO 变更图标的位置
+		menuOpen?openShareMenu(false):closeShareMenu();
 	}
-	openShareMenu();
+	openShareMenu(true);
 	$toggleButton.on("mousedown",function(){
 		toggleShareMenu();
 	})
