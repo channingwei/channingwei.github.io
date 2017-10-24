21c1840d6e867aefcca3ffa563d2a86453fea24b
Channing Kuo <492689363@qq.com>
2017-10-24 17:49:20 +0800

    add image

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,6 +1,12 @@
 
 // 1、left-panel setup background image
-var imageWidth = 1920;
+var imageWidth = 962;
+var documentWidth = $(document.body).width();
 var leftPanelWidth = $(".left-panel").width();
-$(".left-panel").css({'background': 'url(./images/consuegra.jpg) ' + -(imageWidth - leftPanelWidth) + 'px 0 no-repeat'})
-$(".rigth-panel").css({'background': 'url(./images/consuegra.jpg) 0 0 no-repeat'})
+$(".left-panel").css({'background': 'url(./images/consuegra-left.jpg) ' + -(documentWidth / 2 - leftPanelWidth) + 'px 0 no-repeat'})
+$(".rigth-panel").css({'background': 'url(./images/consuegra-right.jpg) 0 0 no-repeat'})
+
+setInterval(function(){
+    // TODO change background
+
+}, 1000);
