c0d838ce7a8b4cd04e1a8fd2dc50d2b83270535d
ChanningKuo <492689363@qq.com>
2018-01-09 13:12:38 +0800

    准备添加首页文章鼠标移到上面3秒后加载预览的效果

---
+++ src/components/module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// import { PopUpService } from './service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { Container, ContainerHostDirective } from './base/index';
+// import {
+//     DialogContainerComponent,
+//     DialogComponent,
+//     ToastContainerComponent,
+//     ToastComponent,
+//     LoadContainerComponent,
+//     LoadComponent
+// } from './interface/index';
+
+@NgModule({
+    declarations: [
+        // ContainerHostDirective,
+
+        // DialogComponent,
+        // DialogContainerComponent,
+
+        // ToastContainerComponent,
+        // ToastComponent,
+
+        // LoadContainerComponent,
+        // LoadComponent
+    ],
+    imports: [BrowserAnimationsModule, CommonModule],
+    exports: [],
+    providers: [/*PopUpService, Container*/],
+    entryComponents: [
+        // DialogComponent,
+        // DialogContainerComponent,
+
+        // ToastContainerComponent,
+        // ToastComponent,
+
+        // LoadContainerComponent,
+        // LoadComponent
+    ]
+})
+export class KuoModule { }
